97c025b8e14204f520ec6dd0b683d4485061971f
Robbe Van Roey <11801715@student.pxl.be>
2019-03-23 17:42:28 +0100

    Module3: Done!

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,6 +1,7 @@
 ﻿using GradeBook.Enums;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace GradeBook.GradeBooks
@@ -11,6 +12,39 @@ namespace GradeBook.GradeBooks
 			:base(name)
 		{
 			Type = GradeBookType.Ranked;
+		}
+
+		public override char GetLetterGrade(double averageGrade)
+		{
+			if (Students.Count < 5)
+			{
+				throw new InvalidOperationException();
+			}
+			else
+			{
+				int threshold = (int)Math.Ceiling(Students.Count * .2);
+				List<double> grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
+
+				if (grades[threshold - 1] <= averageGrade)
+				{
+					return 'A';
+				}
+				else if (grades[2 * threshold - 1] <= averageGrade)
+				{
+					return 'B';
+				}
+				else if (grades[3 * threshold - 1] <= averageGrade)
+				{
+					return 'C';
+				}
+				else if (grades[4 * threshold - 1] <= averageGrade)
+				{
+					return 'D';
+				} else
+				{
+					return 'F';
+				}
+			}
 		}
 	}
 }
